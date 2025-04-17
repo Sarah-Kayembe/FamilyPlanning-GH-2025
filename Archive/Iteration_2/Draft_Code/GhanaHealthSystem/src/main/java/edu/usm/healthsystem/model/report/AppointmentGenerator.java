08ebd979c705accf51d28b5a2a41a4d5944a0f3a
Sarah Lawrence <sarahlawrence9927@gmail.com>
2025-04-17 14:11:35 -0400

    Cleaning up files

--- Archive/Iteration_2/Draft_Code/GhanaHealthSystem/src/main/java/edu/usm/healthsystem/model/report/AppointmentGenerator.java
+++
@@ -1,9 +0,0 @@
-package edu.usm.healthsystem.model.report;
-
-import java.util.List;
-
-public class AppointmentGenerator {
-    public static void addInfo(List<String[]> report) {
-        
-    }
-}
