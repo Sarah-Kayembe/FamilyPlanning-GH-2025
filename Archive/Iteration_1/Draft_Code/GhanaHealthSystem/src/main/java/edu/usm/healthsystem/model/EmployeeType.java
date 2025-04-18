b93bcd7612cb5e13ecb4f3409c9a3f1c86ae923d
Sarah-Kayembe <158983142+Sarah-Kayembe@users.noreply.github.com>
2025-04-17 23:32:23 -0400

    Revert "Iteration 2: merging Iteration_2 branch into main branch Iteration_2 folder."

---
+++ Archive/Iteration_1/Draft_Code/GhanaHealthSystem/src/main/java/edu/usm/healthsystem/model/EmployeeType.java
@@ -0,0 +1,10 @@
+package edu.usm.healthsystem.model;
+
+/**
+ * Enum representing different types of employees in the health system.
+ */
+public enum EmployeeType {
+    NURSE,
+    SYSTEM_ADMIN,
+    NURSE_SUPERVISOR
+}
