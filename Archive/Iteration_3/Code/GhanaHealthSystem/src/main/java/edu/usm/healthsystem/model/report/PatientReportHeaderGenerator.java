52c9908d59c2f4cfaafc731edb6e5cb035caeef3
SarahYLawrence <148221524+SarahYLawrence@users.noreply.github.com>
2025-05-02 00:59:48 -0400

    Add files via upload

--- Archive/Iteration_3/Code/GhanaHealthSystem/src/main/java/edu/usm/healthsystem/model/report/PatientReportHeaderGenerator.java
+++ Archive/Iteration_3/Code/GhanaHealthSystem/src/main/java/edu/usm/healthsystem/model/report/PatientReportHeaderGenerator.java
@@ -17,23 +17,30 @@ public class PatientReportHeaderGenerator {
      */
     public static List<String[]> addHeader(String year, String facility, String subDistrict, String district) {
         List<String[]> header = new ArrayList<>();
-    
-        header.add(new String[]{"FAMILY PLANNING CLIENT REGISTER"});
-        header.add(new String[]{"YEAR: " + year});
-        header.add(new String[]{"FACILITY / ZONE: " + facility +
-                "    Sub-district: " + subDistrict +
-                "    District: " + district});
-        header.add(new String[]{""}); // Empty line
-    
-        // Now add table header: each entry is a true CSV column
+
+        header.add(new String[]{
+                "FAMILY PLANNING CLIENT REGISTER", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+        });
+
+        header.add(new String[]{
+                "YEAR:", year, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+        });
+
+        header.add(new String[]{
+                "FACILITY / ZONE:", facility,
+                "Sub-district:", subDistrict,
+                "District:", district,
+                "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+        });
+
         header.add(new String[]{
                 "S/NO", "Date", "NHIS Reg. No.", "Card No", "NAME",
                 "Marital Status", "Sex", "Address (Location/WP/Community/Hse No.)",
                 "Method of Choice", "1st ever use of method (Y/N)", "Age", "Parity",
                 "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
         });
-        
+
         return header;
-    }    
+    }
     
 }
